c00a74a7b766ceef3b70bd4bceb36fa6b579833e
Jan Carlos Martinez <jan.martinez1@upr.edu>
2020-01-28 02:29:46 -0500

    first commit

---
+++ client/backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  imagePath: {
+    type: String,
+    require: true
+  }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
